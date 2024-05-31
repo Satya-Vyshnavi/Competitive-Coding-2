49cbb24ae6263970555d90914a6f5509c41e26d4
unknown <vyshnavi.chakka@gmail.com>
2024-05-31 14:14:10 -0400

    Done Competitive-Coding-2

---
+++ Problem1.java
@@ -0,0 +1,19 @@
+T.c --> O(n)
+S.c --> O(n)
+// Two Sum Problem
+
+class Solution {
+    public int[] twoSum(int[] nums, int target) {        
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for(int i =0; i<nums.length; i++){
+          map.put(nums[i],i);
+        }
+        for(int i =0; i<nums.length; i++){
+            int diff = target - nums[i];
+            if(map.containsKey(diff) && i != map.get(diff)){
+               return new int[]{map.get(diff), i}; 
+            }
+        }
+        return new int[]{-1,-1};
+    }
+}
